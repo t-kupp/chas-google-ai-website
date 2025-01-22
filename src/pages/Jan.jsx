23916767907e5a57df2f2c0f281e54607191387c
t-kupp <kupper.thorge@gmail.com>
2025-01-22 17:34:34 +0100

    added simple prompt system

--- src/pages/Jan.jsx
+++ src/pages/Jan.jsx
@@ -4,35 +4,47 @@ import { model } from "../../util/ai";
 import { useState } from "react";
 
 export default function ChasGPT() {
-  const aiResponse = "Test";
   const [prompt, setPrompt] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [currentConversation, setCurrentConversation] = [
+    {
+      prompt: "",
+      answer: "",
+    },
+  ];
 
   function handleKeyDown(e) {
     if (e.key === "Enter" && prompt.trim() != "") {
-      console.log(prompt);
+      sendPrompt(prompt);
       setPrompt("");
       e.target.value = "";
     }
   }
 
+  async function sendPrompt() {
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+    setAnswer(result.response.text());
+  }
+
   return (
-    <div className="flex min-h-[90vh] flex-col">
+    <div className="flex min-h-[90vh] flex-col p-4">
       <div className="navbar">
         <Sidebar />
         <button className="btn btn-ghost">ChasGPT</button>
       </div>
-      <div className="mx-auto flex w-full max-w-3xl flex-grow flex-col items-center justify-center">
-        <div>
-          <p>{aiResponse}</p>
-        </div>
-        <div className="flex w-full flex-col items-center">
+      <div className="mx-auto flex w-full max-w-3xl flex-grow flex-col">
+        <div className="mb-12 mt-auto flex flex-col items-center gap-8">
+          <div>
+            <p>{answer}</p>
+          </div>
           <h1 className="mb-8 text-3xl font-bold">What can I help with?</h1>
           <input
             onChange={(e) => setPrompt(e.target.value)}
             onKeyDown={(e) => handleKeyDown(e)}
             type="text"
             placeholder="Message ChasGPT"
-            className="input input-lg input-bordered size w-full"
+            className="input input-lg input-bordered w-full"
           />
         </div>
       </div>
