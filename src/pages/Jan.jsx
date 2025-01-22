2b12b119d78dcf303de82bad0023cf652d3b90a0
t-kupp <kupper.thorge@gmail.com>
2025-01-22 19:32:17 +0100

    added basic chatting function

--- src/pages/Jan.jsx
+++ src/pages/Jan.jsx
@@ -6,16 +6,12 @@ import { useState } from "react";
 export default function ChasGPT() {
   const [prompt, setPrompt] = useState("");
   const [answer, setAnswer] = useState("");
-  const [currentConversation, setCurrentConversation] = [
-    {
-      prompt: "",
-      answer: "",
-    },
-  ];
+  const [displayPrompt, setDisplayPrompt] = useState("");
 
   function handleKeyDown(e) {
     if (e.key === "Enter" && prompt.trim() != "") {
       sendPrompt(prompt);
+      setDisplayPrompt(prompt);
       setPrompt("");
       e.target.value = "";
     }
@@ -28,23 +24,34 @@ export default function ChasGPT() {
   }
 
   return (
-    <div className="flex min-h-[90vh] flex-col p-4">
+    <div className="flex min-h-[90vh] flex-col px-4 py-8">
       <div className="navbar">
         <Sidebar />
         <button className="btn btn-ghost">ChasGPT</button>
       </div>
       <div className="mx-auto flex w-full max-w-3xl flex-grow flex-col">
-        <div className="mb-12 mt-auto flex flex-col items-center gap-8">
-          <div>
-            <p>{answer}</p>
+        <div className="flex flex-col gap-4">
+          <div className="chat chat-start">
+            {displayPrompt && (
+              <div className="chat-bubble">{displayPrompt}</div>
+            )}
           </div>
-          <h1 className="mb-8 text-3xl font-bold">What can I help with?</h1>
+          <div className="chat chat-end">
+            {answer && <div className="chat-bubble">{answer}</div>}
+          </div>
+        </div>
+        <div className="mt-auto flex flex-col">
+          {!displayPrompt && (
+            <h1 className="mx-auto my-8 text-2xl font-bold">
+              What can I help with?
+            </h1>
+          )}
           <input
             onChange={(e) => setPrompt(e.target.value)}
             onKeyDown={(e) => handleKeyDown(e)}
             type="text"
             placeholder="Message ChasGPT"
-            className="input input-lg input-bordered w-full"
+            className="input input-bordered mx-auto h-16 w-full max-w-xl"
           />
         </div>
       </div>
