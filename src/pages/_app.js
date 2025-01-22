fbedacbd1b44c751c679ed8bb7e6d4a6f73011a4
t-kupp <kupper.thorge@gmail.com>
2025-01-22 13:23:28 +0100

    added minimum height for page(s) content

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,11 @@ import Header from "@/components/Header";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <div className="min-h-screen">
       <Header />
       <div class="w-full">
         <Component {...pageProps} />
       </div>
-    </>
+    </div>
   );
 }
