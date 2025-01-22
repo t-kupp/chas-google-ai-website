9ba87534e2852417ef3bc1dad05a3ddb413bc9b5
t-kupp <kupper.thorge@gmail.com>
2025-01-22 14:24:45 +0100

    added react-icons and created sidebar component

--- src/pages/Jan.jsx
+++ src/pages/Jan.jsx
@@ -1,13 +1,18 @@
+import Sidebar from "@/components/Jan-Sidebar";
+
 export default function ChasGPT() {
   return (
-    <div className="mx-auto w-full max-w-3xl">
-      <div className="flex w-full flex-col items-center justify-center">
-        <h1>ChasGPT</h1>
-        <h1>What can I help with?</h1>
+    <div className="flex min-h-[90vh] flex-col">
+      <div className="navbar">
+        <Sidebar />
+        <button className="btn btn-ghost">ChasGPT</button>
+      </div>
+      <div className="mx-auto flex w-full max-w-3xl flex-grow flex-col items-center justify-center">
+        <h1 className="mb-8 text-3xl font-bold">What can I help with?</h1>
         <input
           type="text"
           placeholder="Message ChasGPT"
-          className="input input-bordered w-full"
+          className="input input-lg input-bordered size w-full"
         />
       </div>
     </div>
