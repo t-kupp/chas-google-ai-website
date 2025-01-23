888a5e6ef83fbed0602bbb485eb325cfb8540784
t-kupp <kupper.thorge@gmail.com>
2025-01-23 12:02:05 +0100

    cleaned up the need for promptDisplay useState

--- src/pages/Jan.jsx
+++ src/pages/Jan.jsx
@@ -6,21 +6,20 @@ import { useState } from "react";
 export default function ChasGPT() {
   const [prompt, setPrompt] = useState("");
   const [answer, setAnswer] = useState("");
-  const [displayPrompt, setDisplayPrompt] = useState("");
 
   function handleKeyDown(e) {
-    if (e.key === "Enter" && prompt.trim() != "") {
-      sendPrompt(prompt);
-      setDisplayPrompt(prompt);
-      setPrompt("");
+    if (e.key === "Enter" && e.target.value.trim() != "") {
+      const inputValue = e.target.value;
+      setPrompt(inputValue);
+      sendPrompt(inputValue);
       e.target.value = "";
     }
   }
 
-  async function sendPrompt() {
+  async function sendPrompt(prompt) {
     const result = await model.generateContent(prompt);
-    console.log(result.response.text());
-    setAnswer(result.response.text());
+    const responseText = result.response.text();
+    setAnswer(responseText);
   }
 
   return (
@@ -31,13 +30,12 @@ export default function ChasGPT() {
       </div>
       <div className="mx-auto flex w-full max-w-3xl flex-grow flex-col">
         <div className="flex flex-col gap-4">
-          <div className="chat chat-start">{displayPrompt && <div className="chat-bubble">{displayPrompt}</div>}</div>
+          <div className="chat chat-start">{prompt && <div className="chat-bubble">{prompt}</div>}</div>
           <div className="chat chat-end">{answer && <div className="chat-bubble">{answer}</div>}</div>
         </div>
         <div className="mt-auto flex flex-col">
-          {!displayPrompt && <h1 className="mx-auto my-8 text-2xl font-bold">What can I help with?</h1>}
+          {!prompt && <h1 className="mx-auto my-8 text-2xl font-bold">What can I help with?</h1>}
           <input
-            onChange={(e) => setPrompt(e.target.value)}
             onKeyDown={(e) => handleKeyDown(e)}
             type="text"
             placeholder="Message ChasGPT"
