b33ce99f67cae78e8adbda6b70202813f9034c79
t-kupp <kupper.thorge@gmail.com>
2025-01-23 15:08:24 +0100

    adjusted system instructions

--- util/jan-ai.js
+++ util/jan-ai.js
@@ -4,5 +4,5 @@ const genAI = new GoogleGenerativeAI(apiKey);
 export const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
   systemInstruction:
-    "Your name is ChasGPT and you are located at Chas Academy. You are a chatbot and help the user with all sorts of matters.",
+    "If the user greets you greet them back with your name ChasGPT, otherwise just answer the question. You are a chatbot and help the user with all sorts of matters.",
 });
