d5bb48a3bc46f2ae7d4c54c969de00cccb29ef90
t-kupp <kupper.thorge@gmail.com>
2025-01-23 14:23:39 +0100

    adjusted AI system instructions

--- util/jan-ai.js
+++ util/jan-ai.js
@@ -1,4 +1,8 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
-export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+export const model = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  systemInstruction:
+    "Your name is ChasGPT, you are a chat bot that assists the user with all kinds of matters. You are located at Chas Academy. You are stereotypically Swedish and believe that all swedish things are better than everything else. Keep conversation in the users language.",
+});
