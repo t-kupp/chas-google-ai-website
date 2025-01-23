22d7b1f1441372da3aa5666824ff75b233d7a1d0
t-kupp <kupper.thorge@gmail.com>
2025-01-23 21:12:41 +0100

    added chas logo to the chatbot

--- src/pages/ChasGPT.jsx
+++ src/pages/ChasGPT.jsx
@@ -7,6 +7,10 @@ export default function ChasGPT() {
   const [isThinking, setIsThinking] = useState(false);
 
   function handleKeyDown(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+    }
+
     if (e.key === "Enter" && e.target.value.trim() !== "") {
       const userMessage = e.target.value.trim();
       e.target.value = "";
@@ -51,17 +55,23 @@ export default function ChasGPT() {
   }
 
   return (
-    <div className="flex flex-grow flex-col px-4 py-8 dark:bg-neutral-900">
+    <div className="flex flex-grow flex-col px-4 py-8 leading-relaxed text-gray-800 dark:bg-neutral-900 dark:text-gray-200">
       <div className="navbar">
         <Sidebar />
         <button className="btn btn-ghost">ChasGPT</button>
       </div>
       <div className="mx-auto flex w-full max-w-3xl flex-grow flex-col">
         <div className="flex max-h-full flex-col gap-4">
-          {console.log(history)}
           {history.map((entry, index) => (
-            <div key={index} className={`chat ${entry.role === "user" ? "chat-end" : "chat-start"}`}>
-              <div className={`${entry.role === "user" && "chat-bubble"} whitespace-pre-wrap`}>
+            <div key={index} className={`chat gap-1 ${entry.role === "user" ? "chat-end" : "chat-start"}`}>
+              {entry.role === "model" && (
+                <div className="avatar chat-image self-start p-1">
+                  <div className="w-8 rounded-full">
+                    <img src="/chas-logo-small.png" alt="Chas Logo" />
+                  </div>
+                </div>
+              )}
+              <div className={`${entry.role === "user" ? "chat-bubble" : ""} whitespace-pre-wrap px-4 py-2`}>
                 {entry.parts.map((part) => processText(part.text))}
               </div>
             </div>
@@ -70,11 +80,11 @@ export default function ChasGPT() {
         </div>
         <div className="sticky bottom-8 mt-auto flex flex-col pt-8">
           {history.length == 0 && <h1 className="mx-auto my-8 text-2xl font-bold">What can I help with?</h1>}{" "}
-          <input
+          <textarea
             onKeyDown={(e) => handleKeyDown(e)}
             type="text"
             placeholder="Message ChasGPT"
-            className="input input-bordered mx-auto h-16 w-full max-w-xl rounded-3xl dark:bg-neutral-800 dark:placeholder-neutral-400"
+            className="textarea mx-auto !h-24 w-full resize-none rounded-3xl border-none bg-neutral-100 px-5 py-3 text-base placeholder-neutral-600 dark:bg-neutral-800 dark:placeholder-neutral-400"
           />
         </div>
       </div>
