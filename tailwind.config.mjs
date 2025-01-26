1725662a5b659c3f01baf340f996c708a3e5e7f3
t-kupp <kupper.thorge@gmail.com>
2025-01-26 10:55:37 +0100

    removed default next.js background colors for DaisyUI theming

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,14 +5,7 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
+  theme: {},
   daisyui: {
     themes: ["light", "dark"],
   },
